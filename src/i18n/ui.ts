c6e4fe335e7cdbb9fd44aa230129e4cc476e0b67
Romu T. <r@tisserand.eu>
2025-05-02 15:53:03 +0200

    First basic i18n version.

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: "English GB",
+  fr: "Français - France",
+};
+
+export const defaultLang = "fr";
+
+export const ui = {
+  en: {
+    "home.message": "Pizza is a science, don't cheat with science.",
+    "nav.about": "About",
+  },
+  fr: {
+    "home.message":
+      "La pizza est une science, on ne rigole pas avec la science.",
+    "nav.about": "À propos",
+  },
+} as const;
