b2fc44774dd726da6f714472269f94e898133ceb
Romu T. <r@tisserand.eu>
2025-05-11 11:59:32 +0200

    Fixed some things related to path. And added log messages for debugging.

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,16 +1,20 @@
 import { ui, defaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  // console.log("url: ", url);
-  // It'll need some rework when the site gets published with the final domain name.
-  // TODO
-  const [, , lang] = url.pathname.split("/");
+  // console.log("url: ", url.href);
+  const [, lang] = url.pathname.split("/");
   // console.log("lang: ", lang);
-  if (lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
+  if (lang in ui) {
+    let val = lang as keyof typeof ui;
+    // console.log("val: ", val);
+    return val;
+  } else {
+    return defaultLang;
+  }
 }
 
 export function useTranslations(lang: keyof typeof ui) {
+  // console.log("useTranslations: ", lang);
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   };
