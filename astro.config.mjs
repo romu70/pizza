9c855d0b6590203c885d2d213c2a5610cb07418d
Romu T. <r@tisserand.eu>
2025-06-06 17:51:04 +0200

    Added the necessary to get the robots and sitemap files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://romu70.github.io",
+  site: "https://coach.pizza",
+  integrations: [sitemap()],
 });
